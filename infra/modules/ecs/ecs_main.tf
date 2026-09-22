2e6f81883183294f1604b13af3679e11ab816d60
Yasir Moosa <yasiryousafmoosa@gmail.com>
2026-09-22 23:37:37 +0100

    change to comment

--- infra/modules/ecs/ecs_main.tf
+++ infra/modules/ecs/ecs_main.tf
@@ -38,7 +38,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-# Attaching policy to role
+# Attaching AWS policy to role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
